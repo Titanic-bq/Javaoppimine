12b211b4becc8298b5098373f8b1ac8ba6999c34
hendrik <hendrik.jurgenson@voco.ee>
2025-10-30 09:12:54 +0200

    töö

---
+++ massiivid.js
@@ -0,0 +1,24 @@
+
+
+let fruits= ["apple","orange","banana","cocunut"];
+
+fruits.sort();  //sorteeribe tähestilikus järjekorras
+fruits.sort().reverse();  //sordib tagurpidi
+//fruits.push("cocunut");
+//fruits.pop();
+//fruits.unshift("mango");
+//fruits.shift();
+
+let numOFruits = fruits.length; //peab olema ilma sulgudetta
+
+let index = fruits.indexOf("apple");  //kui ei ole loetelus, siis väärtus on -1
+
+for(let i = fruits.length; 1 >= 0 ; i--){
+    console.log(fruits[i]);
+    
+}
+
+//console.log(fruits[0]);
+//console.log(fruits[1]);
+//console.log(fruits[2]);
+//console.log(fruits[3]);
